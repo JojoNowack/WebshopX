f1d5013274dfc575fc261ffa47bf7e4acdccb209
Johannes Nowack <johannes.nowack@mail.de>
2022-11-15 12:09:14 +0100

    init

---
+++ src/main/java/de/oosd/java/WebsshopX/WebsshopXApplication.java
@@ -0,0 +1,13 @@
+package de.oosd.java.WebsshopX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsshopXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsshopXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/oosd/java/WebsshopX/WebsshopXApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oosd.java.WebsshopX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsshopXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
