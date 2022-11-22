60cc2953c9bf93466288e23f55b95f0427620bec
Johannes Nowack <johannes.nowack@mail.de>
2022-11-22 11:35:40 +0100

    website läuft

---
+++ src/main/java/de/hsa/OOSD/WebshopX/webshopx/HelloController.java
@@ -0,0 +1,17 @@
+package de.hsa.OOSD.WebshopX.webshopx;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String home()  {
+
+        return "index";
+    }
+    @GetMapping("/greeting")
+    public String greeting()  {
+
+        return "greeting";
+    }
+}
