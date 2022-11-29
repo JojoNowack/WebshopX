836a3cb693889335229e3087850cd76200fc294d
Andreas Schmid <andreas.schmid1@hs-augsburg.de>
2022-11-29 11:40:51 +0100

    Add homePage.htlm in order to display our data in the front end

--- src/main/java/de/hsa/OOSD/WebshopX/webshopx/WebshopxApplication.java
+++ src/main/java/de/hsa/OOSD/WebshopX/webshopx/WebshopxApplication.java
@@ -4,12 +4,9 @@ import de.hsa.OOSD.WebshopX.webshopx.models.Product;
 import de.hsa.OOSD.WebshopX.webshopx.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Arrays;
+
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class WebshopxApplication {
