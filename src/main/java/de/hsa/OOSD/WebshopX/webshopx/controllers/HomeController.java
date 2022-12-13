1594ec86d6f0c80307ced19668e5622c2ab2918b
Johannes Nowack <johannes.nowack@mail.de>
2022-12-13 23:19:58 +0100

    is working again

--- src/main/java/de/hsa/OOSD/WebshopX/webshopx/controllers/HomeController.java
+++ src/main/java/de/hsa/OOSD/WebshopX/webshopx/controllers/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 
 
     @GetMapping("/")
-    public String home(Model model, @Param("keyword") String keyword, @Param("order") String order) {
+    public String home(Model model, @Param("keyword") String keyword) {
 
         //System.out.println(keyword);
         //System.out.println(order);
--- src/main/java/de/hsa/OOSD/WebshopX/webshopx/controllers/ProductController.java
+++ src/main/java/de/hsa/OOSD/WebshopX/webshopx/controllers/ProductController.java
@@ -3,14 +3,12 @@ package de.hsa.OOSD.WebshopX.webshopx.controllers;
 import de.hsa.OOSD.WebshopX.webshopx.models.Product;
 import de.hsa.OOSD.WebshopX.webshopx.services.CategoryService;
 import de.hsa.OOSD.WebshopX.webshopx.services.ProductService;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 
 @Controller
@@ -25,30 +23,9 @@ public class ProductController {
     }
 
 
-    @GetMapping("/")
-    public String home(Model model, @Param("keyword") String keyword, @Param("order") String order) {
 
-        //System.out.println(keyword);
-        //System.out.println(order);
-
-        Iterable<Product> filteredProducts = productService.getFilteredProducts(keyword);
-        if (filteredProducts instanceof Collection) {
-            int size = ((Collection<?>) filteredProducts).size();
-            if (size < 1) {
-                //System.out.println("no results");
-                model.addAttribute("noProducts", "Zu dieser Suche konnten leider keine Produkte gefunden werden");
-            }
-        }
-        model.addAttribute("products", filteredProducts);
-        model.addAttribute("categories", categoryService.getAllCategories());
-
-
-        return "home_page_bootstrap5";
-    }
 
     private final ArrayList<Product> costumerProducts = new ArrayList<>();
-
-
 
 
     @GetMapping("/{name}")
